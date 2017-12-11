74e0c02fbef05c8480a355e7404cacfe3b085d54
fatemaislam <islamfatema04@gmail.com>
2017-12-10 20:18:58 -0500

    Added the exercise

---
+++ src/main/java/domTomProject/com/com/domTomProject/App.java
@@ -0,0 +1,13 @@
+package domTomProject.com.com.domTomProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
